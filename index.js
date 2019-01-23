3891d02bc2f5eadd5dd5f225d25ec19f42af6486
Andrew Grathwohl <andrew@grathwohl.me>
2019-01-22 22:59:12 -0500

    wtf

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const process = require('process')
+const serve = require('serve-static')
+
+const app = express()
+
+app.use(serve('public/', {'index': ['playlist.html', 'index.html']}))
+app.use(serve('releases/', { 'index': false }))
+
+app.listen(process.env.PORT || 8080)
