114d0e93c68171f8fb6a5ab158a708bf2ee33b41
Andrew Grathwohl <andrew@grathwohl.me>
2019-01-22 22:56:09 -0500

    fuck it all

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const serve = require('serve-static')
-
-const app = express()
-
-app.use(serve('public/', {'index': ['playlist.html', 'index.html']}))
-app.use(serve('releases/', { 'index': false }))
-
-app.listen(80)
