a0c3ceb4d356e3b2473d2404fe1cbe898358acfd
Andrew Grathwohl <andrew@grathwohl.me>
2019-01-28 21:21:07 -0500

    feed

--- feed/options.js
+++ feed/options.js
@@ -3,27 +3,27 @@ module.exports = {
     title: '▲',
     description: '▲',
     generator: '▲',
-    feedUrl: 'sonicmultiplicities.audio/feed.xml',
-    siteUrl: 'sonicmultiplicities.audio',
-    author: 'Andrew Grathwohl',
-    managingEditor: 'Andrew Grathwohl',
-    copyright: 'I will never acknowleedge copyright law.',
+    feedUrl: 'http://sonicmultiplicities.audio/feed.xml',
+    siteUrl: 'http://sonicmultiplicities.audio',
+    author: '▲',
+    managingEditor: '▲',
+    copyright: 'None',
     language: 'Music',
     categories: ['Music'],
-    itunesOwner: { name: 'Andrew Grathwohl', email:'andrew@grathwohl.me'  },
+    itunesOwner: { name: '▲', email:'andrew@grathwohl.me'  },
   },
   item: {
     title: '▲',
-    description: 'Featuring Joseph Werle and Michael Dinner, acoustic guitars',
+    description: 'Joseph Werle and Michael Dinner, acoustic guitars',
     url: 'http://sonicmultiplicities.audio/▲.mp3',
     guid: '▲',
     categories: ['Music'],
-    author: 'Andrew Grathwohl',
+    author: '▲',
     date: new Date(),
     enclosure: {
-      url: 'http://sonicmultiplicities.audio/opus/▲.opus',
-      file: '../releases/opus/▲.opus',
-      type: 'audio/opus'
+      url: 'http://sonicmultiplicities.audio/▲.mp3',
+      file: '../releases/▲.mp3',
+      type: 'audio/mpeg'
     }
   }
 }
